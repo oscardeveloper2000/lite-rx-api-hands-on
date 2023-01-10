c2a50194c36e17158513a0dd6918e0bae3368872
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-10 18:25:19 -0500

    Punto de control, errorMono
    Se utiliza el operador error para retornar un Mono que emite una IllegalStateException

--- src/main/java/io/pivotal/literx/Part02Mono.java
+++ src/main/java/io/pivotal/literx/Part02Mono.java
@@ -35,7 +35,7 @@ public class Part02Mono {
 
 	// TODO Create a Mono that emits an IllegalStateException
 	Mono<String> errorMono() {
-		return null;
+		return Mono.error(new IllegalStateException());
 	}
 
 }
