29f07ed55eba716cfdb12ff9b97f318568d1a8a6
oscardev <oscar.sanabria.enequipo.ext@ospinternational.com>
2023-01-11 08:18:40 -0500

    punto de control, capitalizeMany

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -50,7 +50,14 @@ public class Part07Errors {
 	// TODO Implement a method that capitalizes each user of the incoming flux using the
 	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(user -> {
+			try {
+				return capitalizeUser(user);
+			}
+			catch (GetOutOfHereException e) {
+				throw Exceptions.propagate(e);
+			}
+		});
 	}
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
