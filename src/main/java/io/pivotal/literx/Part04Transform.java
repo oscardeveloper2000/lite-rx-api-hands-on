d6ed205cda3da088c105a21245ab0eb82cf652bd
Valentina Santa Muñoz <valentinas792 @gmail.com>
2023-01-10 19:21:25 -0500

    Punto de control, capitalizeOne
    Se utiliza el operador map para transformar y poder poner en mayúsculas el nombre de usuario, nombre y apellido

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -15,7 +15,10 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(u ->
+				new User(u.getUsername().toUpperCase(),
+						u.getFirstname().toUpperCase(),
+						u.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
